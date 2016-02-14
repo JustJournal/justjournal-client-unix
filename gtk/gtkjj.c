b3ea634e9df437eacdf9b775c1b81ea1ee37eb52
Lucas Holt <luke@foolishgames.com>
2016-02-14 14:01:19 -0500

    set default window size.

--- gtk/gtkjj.c
+++ gtk/gtkjj.c
@@ -62,7 +62,7 @@ int main( int argc, char *argv[] )
     window = gtk_window_new( GTK_WINDOW_TOPLEVEL );
     gtk_window_set_title( GTK_WINDOW (window), NAME );
     gtk_container_set_border_width( GTK_CONTAINER (window), 10 );
-    //gtk_widget_set_size_request( window, 200, -1 );
+    gtk_widget_set_size_request( window, 400, 400 );
 
     g_signal_connect (G_OBJECT (window), "destroy",
                   G_CALLBACK (gtk_main_quit), NULL);
